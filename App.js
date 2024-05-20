6f1a6f40931648970545588fb301e47dac701d52
youjin <kujin821@naver.com>
2024-05-20 10:27:30 +0900

    ...

---
+++ App.js
@@ -0,0 +1,23 @@
+    import { enableScreens } from 'react-native-screens';
+    enableScreens();
+
+    import React from 'react';
+    import { NavigationContainer } from '@react-navigation/native';
+    import { createNativeStackNavigator } from '@react-navigation/native-stack';
+    import Start from './screens/start';
+    import LoginAndSignupScreen from './screens/LoginAndSignupScreen';
+
+    const Stack = createNativeStackNavigator();
+
+    const App = () => {
+    return (
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+            <Stack.Screen name="Start" component={Start} />
+            <Stack.Screen name="LoginAndSignupScreen" component={LoginAndSignupScreen} />
+        </Stack.Navigator>
+        </NavigationContainer>
+    );
+    };
+
+    export default App;
